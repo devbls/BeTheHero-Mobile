8835496bc9e0687fda0c5d0362c247acc6226265
devbls <devblsds@gmail.com>
2020-03-28 23:30:33 +0000

    feat: connect to api

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import { Image, TouchableOpacity, Linking } from 'react-native';
 import * as MailComposer from 'expo-mail-composer';
@@ -22,8 +22,18 @@ import logoImg from '../../assets/logo.png';
 
 export default function Details() {
   const { goBack } = useNavigation();
-  const message =
-    'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$120,00';
+  const route = useRoute();
+
+  const { incident } = route.params;
+
+  const message = `Olá ${
+    incident.name
+  }, estou entrando em contato pois gostaria de ajudar no caso "${
+    incident.title
+  }" com o valor de ${Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(incident.value)}`;
 
   function handleNavigateBack() {
     goBack('Incidents');
@@ -31,14 +41,16 @@ export default function Details() {
 
   function sendEmail() {
     MailComposer.composeAsync({
-      subject: 'Herói do caso: Cadelinha atropelada',
-      recipients: ['bluis1707@gmail.com'],
+      subject: `Herói do caso: ${incident.title}`,
+      recipients: [incident.email],
       body: message,
     });
   }
 
   function sendWhatsApp() {
-    Linking.openURL(`whatsapp://send?phone=351932293391&text=${message}`);
+    Linking.openURL(
+      `whatsapp://send?phone=${incident.whatsapp}&text=${message}`
+    );
   }
 
   return (
@@ -51,13 +63,20 @@ export default function Details() {
       </Header>
       <IncidentDetail>
         <IncidentProperty>ONG:</IncidentProperty>
-        <IncidentValue>APAD</IncidentValue>
+        <IncidentValue>
+          {incident.name} de {incident.city}/{incident.uf}
+        </IncidentValue>
 
         <IncidentProperty>CASO:</IncidentProperty>
-        <IncidentValue>Cadelinha atropelada</IncidentValue>
+        <IncidentValue>{incident.title}</IncidentValue>
 
         <IncidentProperty>VALOR:</IncidentProperty>
-        <IncidentValue>R$120,00</IncidentValue>
+        <IncidentValue>
+          {Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(incident.value)}
+        </IncidentValue>
       </IncidentDetail>
 
       <Contacts>
--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { Image, FlatList } from 'react-native';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -20,18 +22,47 @@ import {
 import logoImg from '../../assets/logo.png';
 
 export default function Incidents() {
+  const [incidents, setIncidents] = useState([]);
+  const [totalIncidents, setTotalIncidents] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
   const { navigate } = useNavigation();
 
-  function navigateToDetails() {
-    navigate('Details');
+  function navigateToDetails(incident) {
+    navigate('Details', { incident });
   }
+
+  async function loadIncidents() {
+    if (loading) {
+      return;
+    }
+
+    if (totalIncidents > 0 && incidents.length === totalIncidents) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get('incidents', {
+      params: { page },
+    });
+
+    setIncidents([...incidents, ...response.data]);
+    setTotalIncidents(response.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadIncidents();
+  });
 
   return (
     <Container>
       <Header>
         <Image source={logoImg} />
         <TotalCases>
-          Total de <TotalCasesBold>2 casos</TotalCasesBold>
+          Total de <TotalCasesBold>{totalIncidents} casos</TotalCasesBold>
         </TotalCases>
       </Header>
 
@@ -40,21 +71,28 @@ export default function Incidents() {
 
       <FlatList
         style={{ marginTop: 32 }}
-        data={[1, 2]}
-        keyExtractor={(incident) => String(incident)}
+        data={incidents}
+        keyExtractor={(incident) => String(incident.id)}
         showsVerticalScrollIndicator={false}
-        renderItem={() => (
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
+        renderItem={({ item: incident }) => (
           <IncidentItem>
-            <IncidentProperty />
-            <IncidentValue />
+            <IncidentProperty>ONG:</IncidentProperty>
+            <IncidentValue>{incident.name}</IncidentValue>
 
-            <IncidentProperty />
-            <IncidentValue />
+            <IncidentProperty>CASO:</IncidentProperty>
+            <IncidentValue>{incident.title}</IncidentValue>
 
-            <IncidentProperty />
-            <IncidentValue />
+            <IncidentProperty>VALOR:</IncidentProperty>
+            <IncidentValue>
+              {Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(incident.value)}
+            </IncidentValue>
 
-            <IncidentDetails onPress={navigateToDetails}>
+            <IncidentDetails onPress={() => navigateToDetails(incident)}>
               <IncidentDetailsText>Ver mais detalhes</IncidentDetailsText>
               <Feather name="arrow-right" size={16} color="#E02041" />
             </IncidentDetails>

5bc0c1a512b72241695cf7f604aa6b100996567d
devbls <devblsds@gmail.com>
2020-03-28 22:52:11 +0000

    feat: show incident details and contacts

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,8 +1,78 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+import { Image, TouchableOpacity, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  IncidentDetail,
+  IncidentProperty,
+  IncidentValue,
+  Contacts,
+  ContactsTitle,
+  ContactsDescription,
+  ContactsButtons,
+  ContactButton,
+  ContactButtonText,
+} from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 export default function Details() {
-  return <View />;
+  const { goBack } = useNavigation();
+  const message =
+    'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$120,00';
+
+  function handleNavigateBack() {
+    goBack('Incidents');
+  }
+
+  function sendEmail() {
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: Cadelinha atropelada',
+      recipients: ['bluis1707@gmail.com'],
+      body: message,
+    });
+  }
+
+  function sendWhatsApp() {
+    Linking.openURL(`whatsapp://send?phone=351932293391&text=${message}`);
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Image source={logoImg} />
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Feather name="arrow-left" size={28} color="#E02041" />
+        </TouchableOpacity>
+      </Header>
+      <IncidentDetail>
+        <IncidentProperty>ONG:</IncidentProperty>
+        <IncidentValue>APAD</IncidentValue>
+
+        <IncidentProperty>CASO:</IncidentProperty>
+        <IncidentValue>Cadelinha atropelada</IncidentValue>
+
+        <IncidentProperty>VALOR:</IncidentProperty>
+        <IncidentValue>R$120,00</IncidentValue>
+      </IncidentDetail>
+
+      <Contacts>
+        <ContactsTitle>Salve o dia!</ContactsTitle>
+        <ContactsTitle>Seja o herói desse caso!</ContactsTitle>
+        <ContactsDescription>Entre em contato:</ContactsDescription>
+        <ContactsButtons>
+          <ContactButton onPress={sendWhatsApp}>
+            <ContactButtonText>WhatsApp</ContactButtonText>
+          </ContactButton>
+          <ContactButton onPress={sendEmail}>
+            <ContactButtonText>E-mail</ContactButtonText>
+          </ContactButton>
+        </ContactsButtons>
+      </Contacts>
+    </Container>
+  );
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,3 +1,76 @@
 import styled from 'styled-components/native';
+import Constants from 'expo-constants';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  padding-top: ${Constants.statusBarHeight + 20};
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const IncidentDetail = styled.View`
+  padding: 24px;
+  border-radius: 8px;
+  background-color: #fff;
+  margin-bottom: 16px;
+  margin-top: 48px;
+`;
+
+export const IncidentProperty = styled.Text`
+  font-size: 14px;
+  color: #41414d;
+  font-weight: bold;
+`;
+
+export const IncidentValue = styled.Text`
+  margin-top: 8px;
+  font-size: 15px;
+  margin-bottom: 24px;
+  color: #737380;
+`;
+
+export const Contacts = styled.View`
+  padding: 24px;
+  border-radius: 8px;
+  background-color: #fff;
+  margin-bottom: 16px;
+`;
+
+export const ContactsTitle = styled.Text`
+  font-weight: bold;
+  font-size: 20px;
+  color: #13131a;
+  line-height: 30px;
+`;
+
+export const ContactsDescription = styled.Text`
+  font-size: 15px;
+  color: #737380;
+  margin-top: 16px;
+`;
+
+export const ContactsButtons = styled.View`
+  margin-top: 16px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const ContactButton = styled.TouchableOpacity`
+  background-color: #e02041;
+  border-radius: 8px;
+  height: 50px;
+  width: 48%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContactButtonText = styled.Text`
+  color: #fff;
+  font-size: 15;
+  font-weight: bold;
+`;
